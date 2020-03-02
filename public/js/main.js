8c1aa50248c87681107ff325d35b94ea8be0cf73
Juanvc24 <Juanvc24@users.noreply.github.com>
2020-03-02 17:17:23 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,25 @@ Vue.component('button-container', {
         displayTurnScreen: function() {
             this.$parent._data.selectedModule = this._props.moduleId
             this.$parent._data.activeScreen   = 'printTurn'
+        },
+        pedirTurno: function () {
+            $.ajax({
+                url: 'localhost:3000/api/newTurn',    
+                method: 'post', 
+                data: 'turno',                
+                dataType: 'json',
+                contentType: "application/json"           
+            }).done((response) => {
+                console.log("algoo"+response)
+            });
         }
     },
     template : `<div class="col-6">
-                    <button v-on:click="displayTurnScreen" class="btn-lg btn-block btn-primary">
+                    <button v-if="moduleId>0"  v-on:click="displayTurnScreen" class="btn-lg btn-block btn-primary">
+                        {{displayText}} {{moduleId > 0 ? moduleId : ''}}
+                    </button>
+
+                    <button v-else v-on:click="pedirTurno" class="btn-lg btn-block btn-primary">
                         {{displayText}} {{moduleId > 0 ? moduleId : ''}}
                     </button>
                 </div>`
@@ -20,4 +35,12 @@ const app = new Vue({
         activeScreen   : 'home'
     }
 })
+
+var turno = new Vue({
+        "user": "javier",
+        "turnType": "qr",
+        "module": "A",
+  })
+
+
 
