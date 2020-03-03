b705d419710d58a3f4abdc1fe8f529fa5d4429e0
oscar <javier45x@gmail.com>
2020-03-02 21:08:24 -0500

    cambios en clase

--- public/js/main.js
+++ public/js/main.js
@@ -4,26 +4,39 @@ Vue.component('button-container', {
         displayTurnScreen: function() {
             this.$parent._data.selectedModule = this._props.moduleId
             this.$parent._data.activeScreen   = 'printTurn'
-        },
+        }
+    },
+    template : `<div class="col-6">
+                    <button  v-on:click="displayTurnScreen" class="btn-lg btn-block btn-primary">
+                        {{displayText}} {{moduleId}}
+                    </button>
+                </div>`
+})
+
+Vue.component('button-get-turn', {
+    props    : ['displayText', 'turnType'],
+    methods  : {
         pedirTurno: function () {
+            const sendData = {
+                user: '1121946367',
+                turnType: this.turnType,
+                module: this.$parent._data.selectedModule
+            }
+            console.log('SendData', sendData)
             $.ajax({
-                url: 'localhost:3000/api/newTurn',    
+                url: '127.0.0.1:3000/api/newTurn',    
                 method: 'post', 
-                data: 'turno',                
+                data: JSON.stringify(sendData),                
                 dataType: 'json',
                 contentType: "application/json"           
             }).done((response) => {
-                console.log("algoo"+response)
+                console.log("algoo", response)
             });
         }
     },
     template : `<div class="col-6">
-                    <button v-if="moduleId>0"  v-on:click="displayTurnScreen" class="btn-lg btn-block btn-primary">
-                        {{displayText}} {{moduleId > 0 ? moduleId : ''}}
-                    </button>
-
-                    <button v-else v-on:click="pedirTurno" class="btn-lg btn-block btn-primary">
-                        {{displayText}} {{moduleId > 0 ? moduleId : ''}}
+                    <button  v-on:click="pedirTurno" class="btn-lg btn-block btn-primary">
+                        {{displayText}}
                     </button>
                 </div>`
 })
@@ -31,16 +44,15 @@ Vue.component('button-container', {
 const app = new Vue({
     el: '.app',
     data: {
-        selectedModule : 1,
+        selectedModule : null,
         activeScreen   : 'home'
+    },
+    methods: {
+        displayHomeScreen: function(){
+            this.activeScreen = 'home'
+        },
     }
 })
-
-var turno = new Vue({
-        "user": "javier",
-        "turnType": "qr",
-        "module": "A",
-  })
 
 
 
